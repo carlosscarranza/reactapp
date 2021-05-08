befac1d0f799bb787ff18f91083006f44715dae7
carlosscarranza <58747093+carlosscarranza@users.noreply.github.com>
2021-05-08 12:45:39 -0500

    fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import routes from "./routes/index";
@@ -9,17 +9,17 @@ function App() {
       <div style={{ display: "flex" }}>
         <div style={{ padding: "10px", width: "10%", background: "#f0f0f0" }}>
           <ul style={{ listStyleType: "none", padding: 0 }}>
-            <li><Link to="/example1">example1</Link></li>
-            <li><Link to="/example2">example2</Link></li>
-            <li><Link to="/example3">example3</Link></li>
+            {routes.map((route, index) => (
+              <li>
+                <Link to={route.path}>Example {index + 1}</Link>
+              </li>
+            ))}
           </ul>
         </div>
 
         <div style={{ flex: 1, padding: "10px" }}>
           <Switch>
             {routes.map((route, index) => (
-              // Render more <Route>s with the same paths as
-              // above, but different components this time.
               <Route
                 key={index}
                 path={route.path}
