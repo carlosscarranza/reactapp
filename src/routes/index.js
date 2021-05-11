3b90171b4f19407c9741de8724cd86a52562d8a2
carlosscarranza <58747093+carlosscarranza@users.noreply.github.com>
2021-05-10 19:52:56 -0500

    adding

--- src/routes/index.js
+++ src/routes/index.js
@@ -136,7 +136,62 @@ const routes = [
   },
   {
     path: "/example10",
-    main: () => <div></div>,
+    main: () => (
+      <div>
+        <h1>Types of Food:</h1>
+        <div>
+          <div>
+            <h2>Fruits:</h2>
+            <ul>
+              <li>Apples</li>
+              <li>Blueberries</li>
+              <li>Strawberries</li>
+              <li>Bananas</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      // const TypesOfFruit = () => {
+      //   return (
+      //     <div>
+      //       <h2>Fruits:</h2>
+      //       <ul>
+      //         <li>Apples</li>
+      //         <li>Blueberries</li>
+      //         <li>Strawberries</li>
+      //         <li>Bananas</li>
+      //       </ul>
+      //     </div>
+      //   );
+      // };
+
+      // const Fruits = () => {
+      //   return (
+      //     <div>
+      //       { /* Change code below this line */ }
+      // <TypesOfFruit />
+      //       { /* Change code above this line */ }
+      //     </div>
+      //   );
+      // };
+
+      // class TypesOfFood extends React.Component {
+      //   constructor(props) {
+      //     super(props);
+      //   }
+
+      //   render() {
+      //     return (
+      //       <div>
+      //         <h1>Types of Food:</h1>
+      //         { /* Change code below this line */ }
+      //           <Fruits />
+      //         { /* Change code above this line */ }
+      //       </div>
+      //     );
+      //   }
+      // };
+    ),
   },
   {
     path: "/example11",
