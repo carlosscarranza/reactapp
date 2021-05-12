918e54d0d2816966f255bc8d07dd502bada41e0c
carlosscarranza <58747093+carlosscarranza@users.noreply.github.com>
2021-05-11 19:37:21 -0500

    add

--- src/routes/index.js
+++ src/routes/index.js
@@ -195,189 +195,679 @@ const routes = [
   },
   {
     path: "/example11",
-    main: () => 
-    (<div></div>),
+    main: () => (
+      <div></div>
+      // class Fruits extends React.Component {
+      //   constructor(props) {
+      //     super(props);
+      //   }
+      //   render() {
+      //     return (
+      //       <div>
+      //         <h2>Fruits:</h2>
+      //         <NonCitrus />
+      //         <Citrus />
+      //       </div>
+      //     );
+      //   }
+      // };
+
+      // class TypesOfFood extends React.Component {
+      //   constructor(props) {
+      //      super(props);
+      //   }
+      //   render() {
+      //     return (
+      //       <div>
+      //         <h1>Types of Food:</h1>
+      //         <Fruits />
+      //         <Vegetables />
+      //       </div>
+      //     );
+      //   }
+      // };
+    ),
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example12",
+    main: () => (
+      <div></div>
+      // class TypesOfFood extends React.Component {
+      //   constructor(props) {
+      //     super(props);
+      //   }
+      //   render() {
+      //     return (
+      //       <div>
+      //         <h1>Types of Food:</h1>
+      //         {/* change code below this line */}
+      //         <Fruits />
+      //         <Vegetables />
+      //         {/* change code above this line */}
+      //       </div>
+      //     );
+      //   }
+      // };
+
+      // // change code below this line
+      // ReactDOM.render(<TypesOfFood />, document.getElementById('challenge-node'))
+    ),
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example13",
+    main: () => (
+      <div id="challenge-node">
+        <h1>My First React Component!</h1>
+      </div>
+      //// change code below this line
+      // class MyComponent extends React.Component{
+      //   constructor(props){
+      //     super(props);
+      //   }
+      //   render(){
+      //     return(
+      //           <div id="challenge-node">
+      //                 <h1>My First React Component!</h1>
+      //           </div>
+      //     );
+      //   }
+      // };
+      // ReactDOM.render(<MyComponent/>, document.getElementById("challenge-node"));
+    ),
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example14",
+    main: () => (
+      <div>
+        <h3>What date is it?</h3>
+        <div>
+          <p>The current date is: {Date()}</p>
+        </div>
+      </div>
+      // const CurrentDate = (props) => {
+      //   return (
+      //     <div>
+      //       <p>The current date is: {props.date}</p>
+      //     </div>
+      //   );
+      // };
+
+      // class Calendar extends React.Component {
+      //   constructor(props) {
+      //     super(props);
+      //   }
+      //   render() {
+      //     return (
+      //       <div>
+      //         <h3>What date is it?</h3>
+      //         <CurrentDate date={Date()} />
+      //       </div>
+      //     );
+      //   }
+      // };
+    ),
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example15",
+    main: () => (
+      <div>
+        <h1>To Do Lists</h1>
+        <h2>Today</h2>
+        <p>{["Walk", "Cook", "Bake"].join(", ")}</p>
+        <h2>Tomorrow</h2>
+        <p>{["Study", "Code", "Eat"].join(", ")}</p>
+      </div>
+      // const List = props => {
+      //   return <p>{props.tasks.join(", ")}</p>;
+      // };
+
+      // class ToDo extends React.Component {
+      //   constructor(props) {
+      //     super(props);
+      //   }
+      //   render() {
+      //     return (
+      //       <div>
+      //         <h1>To Do Lists</h1>
+      //         <h2>Today</h2>
+      //         <List tasks={["Walk", "Cook", "Bake"]} />
+      //         <h2>Tomorrow</h2>
+      //         <List tasks={["Study", "Code", "Eat"]} />
+      //       </div>
+      //     );
+      //   }
+      // }
+    ),
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example16",
+    main: () => (
+      <div>
+        <h1>Shopping Cart Component</h1>
+      </div>
+      // const ShoppingCart = (props) => {
+      //   return (
+      //     <div>
+      //       <h1>Shopping Cart Component</h1>
+      //     </div>
+      //   )
+      // };
+      // // Change code below this line
+      // ShoppingCart.defaultProps = { items: 0 }
+    ),
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example17",
+    main: () => (
+      <h1>Current Quantity of Items in Cart: {10}</h1>
+      // const Items = (props) => {
+      //   return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+      // }
+
+      // Items.defaultProps = {
+      //   quantity: 0
+      // }
+
+      // class ShoppingCart extends React.Component {
+      //   constructor(props) {
+      //     super(props);
+      //   }
+      //   render() {
+      //     { /* Change code below this line */ }
+      //     return <Items quantity={10}/>
+      //     { /* Change code above this line */ }
+      //   }
+      // };
+    ),
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example18",
+    main: () => (
+      <h1>Current Quantity of Items in Cart: {0}</h1>
+      // const Items = (props) => {
+      //   return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+      // };
+
+      // // Change code below this line
+      // Items.propTypes = {
+      //   quantity: PropTypes.number.isRequired
+      // };
+      // // Change code above this line
+
+      // Items.defaultProps = {
+      //   quantity: 0
+      // };
+
+      // class ShoppingCart extends React.Component {
+      //   constructor(props) {
+      //     super(props);
+      //   }
+      //   render() {
+      //     return <Items />
+      //   }
+      // };
+    ),
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example19",
+    main: () => (
+      <div>
+        <h2>Reset Password</h2>
+        <h3>We've generated a new temporary password for you.</h3>
+        <h3>Please reset this password from your account settings ASAP.</h3>
+        {/* Change code below this line */}
+        <p>
+          Your temporary password is: <strong>xxxxxxxx</strong>
+        </p>
+        {/* Change code above this line */}
+      </div>
+      // class ReturnTempPassword extends React.Component {
+      //   constructor(props) {
+      //     super(props);
+
+      //   }
+      //   render() {
+      //     return (
+      //         <div>
+      //             { /* Change code below this line */ }
+      //                <p>Your temporary password is: <strong>{this.props.tempPassword}</strong></p>
+      //             { /* Change code above this line */ }
+      //         </div>
+      //     );
+      //   }
+      // };
+
+      // class ResetPassword extends React.Component {
+      //   constructor(props) {
+      //     super(props);
+
+      //   }
+      //   render() {
+      //     return (
+      //         <div>
+      //           <h2>Reset Password</h2>
+      //           <h3>We've generated a new temporary password for you.</h3>
+      //           <h3>Please reset this password from your account settings ASAP.</h3>
+      //           { /* Change code below this line */ }
+      //             <ReturnTempPassword tempPassword="xxxxxxxx" />
+      //           { /* Change code above this line */ }
+      //         </div>
+      //     );
+      //   }
+      // };
+    ),
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example20",
+    main: () => (
+      <div>CamperBot</div>
+      // class CampSite extends React.Component {
+      //   constructor(props) {
+      //     super(props);
+      //   }
+      //   render() {
+      //     return (
+      //       <div>
+      //         <Camper/>
+      //       </div>
+      //     );
+      //   }
+      // };
+      // // Change code below this line
+      // const Camper = props => <p>{props.name}</p>;
+
+      // Camper.defaultProps = {
+      //   name: "CamperBot"
+      // };
+
+      // Camper.propTypes = {
+      //   name: PropTypes.string.isRequired
+      // };
+    ),
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example21",
+    main: () => (
+      <div>
+        <h1>Name</h1>
+      </div>
+      // class StatefulComponent extends React.Component {
+      //   constructor(props) {
+      //     super(props);
+      //     // initialize state here
+
+      //     this.state = {
+      //       name : "Name"
+      //     }
+
+      //   }
+      //   render() {
+      //     return (
+      //       <div>
+      //         <h1>{this.state.name}</h1>
+      //       </div>
+      //     );
+      //   }
+      // };
+    ),
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example22",
+    main: () => (
+      <h1>freeCodeCamp</h1>
+      // class MyComponent extends React.Component {
+      //   constructor(props) {
+      //     super(props);
+      //     this.state = {
+      //       name: 'freeCodeCamp'
+      //     }
+      //   }
+      //   render() {
+      //     return (
+      //       <div>
+      //         { /* Change code below this line */ }
+      //           <h1>{this.state.name}</h1>
+      //         { /* Change code above this line */ }
+      //       </div>
+      //     );
+      //   }
+      // };
+    ),
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example23",
+    main: () => (
+      <h1>freeCodeCamp</h1>
+      // class MyComponent extends React.Component {
+      //   constructor(props) {
+      //     super(props);
+      //     this.state = {
+      //       name: 'freeCodeCamp'
+      //     }
+      //   }
+      //   render() {
+      //     // change code below this line
+      //     const name = this.state.name;
+      //     // change code above this line
+      //     return (
+      //       <div>
+      //         { /* change code below this line */ }
+      //           <h1>{name}</h1>
+      //         { /* change code above this line */ }
+      //       </div>
+      //     );
+      //   }
+      // };
+    ),
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example24",
+    main: () => (
+      <div>
+        <button>Click Me</button>
+        <h1>React Rocks!</h1>
+      </div>
+
+      // class MyComponent extends React.Component {
+      //   constructor(props) {
+      //     super(props);
+      //     this.state = {
+      //       name: 'Initial State'
+      //     };
+      //     this.handleClick = this.handleClick.bind(this);
+      //   }
+      //   handleClick() {
+      //     // change code below this line
+      //     this.setState({
+      //       name: 'React Rocks!'
+      //     });
+      //     // change code above this line
+      //   }
+      //   render() {
+      //     return (
+      //       <div>
+      //         <button onClick={this.handleClick}>Click Me</button>
+      //         <h1>{this.state.name}</h1>
+      //       </div>
+      //     );
+      //   }
+      // };
+    ),
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example25",
+    main: () => (
+      <div>
+        <button>Click Me</button>
+        <h1>You clicked!</h1>
+      </div>
+      // class MyComponent extends React.Component {
+      //   constructor(props) {
+      //     super(props);
+      //     this.state = {
+      //       text: "Hello"
+      //     };
+      //     this.handleClick = this.handleClick.bind(this);
+      //   }
+      //   handleClick() {
+      //     this.setState({
+      //       text: "You clicked!"
+      //     });
+      //   }
+      //   render() {
+      //     return (
+      //       <div>
+      //         <button onClick = {this.handleClick}>Click Me</button>
+      //         <h1>{this.state.text}</h1>
+      //       </div>
+      //     );
+      //   }
+      // };
+    ),
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example26",
+    main: () => (
+      <div>
+        <button>Click Me</button>
+        <h1>Now you see me!</h1>
+      </div>
+      // class MyComponent extends React.Component {
+      //   constructor(props) {
+      //     super(props);
+      //     this.state = {
+      //       visibility: false
+      //     };
+      //     this.toggleVisibility = this.toggleVisibility.bind(this);
+      //    }
+      //   toggleVisibility() {
+      //     this.setState(state => ({
+      //       visibility: !state.visibility
+      //     }));
+      //   }
+      //   render() {
+      //     if (this.state.visibility) {
+      //       return (
+      //         <div>
+      //           <button onClick = {this.toggleVisibility}>Click Me</button>
+      //           <h1>Now you see me!</h1>
+      //         </div>
+      //       );
+      //     } else {
+      //       return (
+      //         <div>
+      //           <button onClick = {this.toggleVisibility}>Click Me</button>
+      //         </div>
+      //       );
+      //     }
+      //   }
+      // };
+    ),
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example27",
+    main: () => (
+      <div>
+        <button className="inc">Increment!</button>
+        <button className="dec">Decrement!</button>
+        <button className="reset">Reset</button>
+        <h1>Current Count: 0</h1>
+      </div>
+      // class Counter extends React.Component {
+      //   constructor(props) {
+      //     super(props);
+      //     this.state = {
+      //       count: 0
+      //     };
+      //   this.increment = this.increment.bind(this);
+      //   this.decrement = this.decrement.bind(this);
+      //   this.reset = this.reset.bind(this);
+      //  }
+      //   reset() {
+      //     this.setState({
+      //       count: 0
+      //     });
+      //   }
+      //   increment() {
+      //     this.setState(state => ({
+      //       count: state.count + 1
+      //     }));
+      //   }
+      //   decrement() {
+      //     this.setState(state => ({
+      //       count: state.count - 1
+      //     }));
+      //   }
+      //   render() {
+      //     return (
+      //       <div>
+      //         <button className='inc' onClick={this.increment}>Increment!</button>
+      //         <button className='dec' onClick={this.decrement}>Decrement!</button>
+      //         <button className='reset' onClick={this.reset}>Reset</button>
+      //         <h1>Current Count: {this.state.count}</h1>
+      //       </div>
+      //     );
+      //   }
+      // };
+    ),
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example28",
+    main: () => (
+      <div>
+        {/* Change code below this line */}
+        <input />
+        {/* Change code above this line */}
+        <h4>Controlled Input:</h4>
+        <p></p>
+      </div>
+      // class ControlledInput extends React.Component {
+      //   constructor(props) {
+      //     super(props);
+      //     this.state = {
+      //       input: ''
+      //     };
+      //   }
+      //   // Change code below this line
+      //   handleChange(event) {
+      //       this.setState({
+      //         input: event.target.value
+      //       });
+      //   }
+      //   // Change code above this line
+      //   render() {
+      //     return (
+      //       <div>
+      //         { /* Change code below this line */}
+      //           <input value = {this.state.input} onChange = {this.handleChange.bind(this)}/>
+      //         { /* Change code above this line */}
+      //         <h4>Controlled Input:</h4>
+      //         <p>{this.state.input}</p>
+      //       </div>
+      //     );
+      //   }
+      // };
+    ),
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example29",
+    main: () => (
+      <div>
+        <form>
+          <input />
+          <button type="submit">Submit!</button>
+        </form>
+        <h1></h1>
+      </div>
+      // class MyForm extends React.Component {
+      //   constructor(props) {
+      //     super(props);
+      //     this.state = {
+      //       input: '',
+      //       submit: ''
+      //     };
+      //     this.handleChange = this.handleChange.bind(this);
+      //     this.handleSubmit = this.handleSubmit.bind(this);
+      //   }
+      //   handleChange(event) {
+      //     this.setState({
+      //       input: event.target.value
+      //     });
+      //   }
+      //   handleSubmit(event) {
+      //     event.preventDefault()
+      //     this.setState({
+      //       submit: this.state.input
+      //     });
+      //   }
+      //   render() {
+      //     return (
+      //       <div>
+      //         <form onSubmit={this.handleSubmit}>
+      //           <input
+      //             value={this.state.input}
+      //             onChange={this.handleChange} />
+      //           <button type='submit'>Submit!</button>
+      //         </form>
+      //         <h1>{this.state.submit}</h1>
+      //       </div>
+      //     );
+      //   }
+      // };
+    ),
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example30",
+    main: () => <div></div>,
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example20",
+    main: () => <div></div>,
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example20",
+    main: () => <div></div>,
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example20",
+    main: () => <div></div>,
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example20",
+    main: () => <div></div>,
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example20",
+    main: () => <div></div>,
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example20",
+    main: () => <div></div>,
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example20",
+    main: () => <div></div>,
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example20",
+    main: () => <div></div>,
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example20",
+    main: () => <div></div>,
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example20",
+    main: () => <div></div>,
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example20",
+    main: () => <div></div>,
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example20",
+    main: () => <div></div>,
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example20",
+    main: () => <div></div>,
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example20",
+    main: () => <div></div>,
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example20",
+    main: () => <div></div>,
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
+    path: "/example20",
+    main: () => <div></div>,
   },
   {
-    path: "/example11",
-    main: () => 
-    (<div></div>),
-  }
+    path: "/example20",
+    main: () => <div></div>,
+  },
 ];
 
 export default routes;
