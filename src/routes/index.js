c293c70b99f5ac3b9e58bf8929050c7f49e92cb7
carlosscarranza <58747093+carlosscarranza@users.noreply.github.com>
2021-05-13 19:34:42 -0500

    finish

--- src/routes/index.js
+++ src/routes/index.js
@@ -1102,10 +1102,7 @@ const routes = [
     path: "/example38",
     main: () => (
       <div>
-        <input
-          type="text"
-          style={{width: 235, margin: 5}}
-        />
+        <input type="text" style={{ width: 235, margin: 5 }} />
         <br />
         <button>Ask the Magic Eight Ball!</button>
         <br />
@@ -1194,39 +1191,401 @@ const routes = [
   },
   {
     path: "/example39",
-    main: () => <div></div>,
+    main: () => (
+      <div>
+        <button>Toggle Display</button>
+      </div>
+      // class MyComponent extends React.Component {
+      //   constructor(props) {
+      //     super(props);
+      //     this.state = {
+      //       display: true
+      //     }
+      //  this.toggleDisplay = this.toggleDisplay.bind(this);
+      //  }
+      //   toggleDisplay() {
+      //     this.setState({
+      //       display: !this.state.display
+      //     });
+      //   }
+      //   render() {
+      //     // change code below this line
+      //     if (this.state.display) {
+      //       return (
+      //          <div>
+      //            <button onClick={this.toggleDisplay}>Toggle Display</button>
+      //            <h1>Displayed!</h1>
+      //          </div>
+      //       );
+      //     } else {
+      //       return (
+      //         <div>
+      //            <button onClick={this.toggleDisplay}>Toggle Display</button>
+      //          </div>
+      //       );
+      //     }
+      //   }
+      // };
+    ),
   },
   {
     path: "/example40",
-    main: () => <div></div>,
+    main: () => (
+      <div>
+        <button>Toggle Display</button>
+        <h1>Displayed!</h1>
+      </div>
+      // class MyComponent extends React.Component {
+      //   constructor(props) {
+      //     super(props);
+      //     this.state = {
+      //       display: true
+      //     }
+      //  this.toggleDisplay = this.toggleDisplay.bind(this);
+      //   }
+      //   toggleDisplay() {
+      //     this.setState(state => ({
+      //       display: !state.display
+      //     }));
+      //   }
+      //   render() {
+      //     // change code below this line
+      //     return (
+      //        <div>
+      //          <button onClick={this.toggleDisplay}>Toggle Display</button>
+      //          {this.state.display && <h1>Displayed!</h1>}
+      //        </div>
+      //     );
+      //   }
+      // };
+    ),
   },
   {
     path: "/example41",
-    main: () => <div></div>,
+    main: () => (
+      <div>
+        <h3>Enter Your Age to Continue</h3>
+        <input style={{ width: 235, margin: 5 }} type="number" />
+        <br />
+        <button>Submit</button>
+      </div>
+      // const inputStyle = {
+      //   width: 235,
+      //   margin: 5
+      // }
+
+      // class CheckUserAge extends React.Component {
+      //   constructor(props) {
+      //     super(props);
+      //     this.state = {
+      //       userAge: '',
+      //       input: ''
+      //     }
+      //     this.submit = this.submit.bind(this);
+      //     this.handleChange = this.handleChange.bind(this);
+      //   }
+      //   handleChange(e) {
+      //     this.setState({
+      //       input: e.target.value,
+      //       userAge: ''
+      //     });
+      //   }
+      //   submit() {
+      //     this.setState(state => ({
+      //       userAge: state.input
+      //     }));
+      //   }
+      //   render() {
+      //     const buttonOne = <button onClick={this.submit}>Submit</button>;
+      //     const buttonTwo = <button>You May Enter</button>;
+      //     const buttonThree = <button>You Shall Not Pass</button>;
+      //     return (
+      //       <div>
+      //         <h3>Enter Your Age to Continue</h3>
+      //         <input
+      //           style={inputStyle}
+      //           type="number"
+      //           value={this.state.input}
+      //           onChange={this.handleChange} /><br />
+      //           {
+      //           this.state.userAge === ''
+      //             ? buttonOne
+      //             : this.state.userAge >= 18
+      //               ? buttonTwo
+      //               : buttonThree
+      //           }
+      //       </div>
+      //     );
+      //   }
+      // };
+    ),
   },
   {
     path: "/example42",
-    main: () => <div></div>,
+    main: () => (
+      <div>
+        <button>Play Again</button>
+        {/* change code below this line */}
+        <h1>You Win!</h1>
+        {/* change code above this line */}
+        <p>{"Turn: " + 0}</p>
+      </div>
+      // class Results extends React.Component {
+      //   constructor(props) {
+      //     super(props);
+      //   }
+      //   render() {
+      //     return (
+      //       <h1>
+      //       {this.props.fiftyFifty ? "You Win!" : "You Lose!"}
+      //       </h1>
+      //     )
+      //   };
+      // };
+
+      // class GameOfChance extends React.Component {
+      //   constructor(props) {
+      //     super(props);
+      //     this.state = {
+      //       counter: 1
+      //     }
+      //     this.handleClick = this.handleClick.bind(this);
+      //   }
+      //   handleClick() {
+      //     this.setState({
+      //       counter: this.state.counter + 1 // change code here
+      //     });
+      //   }
+      //   render() {
+      //     const expression = Math.random() >= 0.5 ? true : false
+      //     return (
+      //       <div>
+      //         <button onClick={this.handleClick}>Play Again</button>
+      //         { /* change code below this line */ }
+      //         <Results fiftyFifty={expression} />
+      //         { /* change code above this line */ }
+      //         <p>{'Turn: ' + this.state.counter}</p>
+      //       </div>
+      //     );
+      //   }
+      // };
+    ),
   },
   {
     path: "/example43",
-    main: () => <div></div>,
+    main: () => (
+      <div>
+        <h3>Don't Type Too Much:</h3>
+        <input type="text" style={{ border: "1px solid black" }} />
+      </div>
+      // class GateKeeper extends React.Component {
+      //   constructor(props) {
+      //     super(props);
+      //     this.state = {
+      //       input: ''
+      //     };
+      //     this.handleChange = this.handleChange.bind(this);
+      //   }
+      //   handleChange(event) {
+      //     this.setState({ input: event.target.value })
+      //   }
+      //   render() {
+      //     let inputStyle = {
+      //       border: '1px solid black'
+      //     };
+      //     // change code below this line
+      //     if (this.state.input.length > 15) {
+      //       inputStyle.border = '3px solid red';
+      //     }
+      //     // change code above this line
+      //     return (
+      //       <div>
+      //         <h3>Don't Type Too Much:</h3>
+      //         <input
+      //           type="text"
+      //           style={inputStyle}
+      //           value={this.state.input}
+      //           onChange={this.handleChange} />
+      //       </div>
+      //     );
+      //   }
+      // };
+    ),
   },
   {
     path: "/example44",
-    main: () => <div></div>,
+    main: () => (
+      <div>
+        <textarea
+          style={{ width: 235, margin: 5 }}
+          placeholder="Separate Items With Commas"
+        />
+        <br />
+        <button>Create List</button>
+        <h1>My "To Do" List:</h1>
+        <ul></ul>
+      </div>
+      // const textAreaStyles = {
+      //   width: 235,
+      //   margin: 5
+      // };
+
+      // class MyToDoList extends React.Component {
+      //   constructor(props) {
+      //     super(props);
+      //     // change code below this line
+      //     this.state = {
+      //       userInput: '',
+      //       toDoList: []
+      //     }
+      //     // change code above this line
+      //     this.handleSubmit = this.handleSubmit.bind(this);
+      //     this.handleChange = this.handleChange.bind(this);
+      //   }
+      //   handleSubmit() {
+      //     const itemsArray = this.state.userInput.split(',');
+      //     this.setState({
+      //       toDoList: itemsArray
+      //     });
+      //   }
+      //   handleChange(e) {
+      //     this.setState({
+      //       userInput: e.target.value
+      //     });
+      //   }
+      //   render() {
+      //     const items = this.state.toDoList.map(i => <li>{i}</li>); // change code here
+      //     return (
+      //       <div>
+      //         <textarea
+      //           onChange={this.handleChange}
+      //           value={this.state.userInput}
+      //           style={textAreaStyles}
+      //           placeholder="Separate Items With Commas" /><br />
+      //         <button onClick={this.handleSubmit}>Create List</button>
+      //         <h1>My "To Do" List:</h1>
+      //         <ul>
+      //           {items}
+      //         </ul>
+      //       </div>
+      //     );
+      //   }
+      // };
+    ),
   },
   {
     path: "/example45",
-    main: () => <div></div>,
+    main: () => (
+      <div>
+        <h1>Popular Front End JavaScript Frameworks</h1>
+        <ul>
+          <li>React</li>
+          <li>Angular</li>
+          <li>Ember</li>
+          <li>Knockout</li>
+          <li>Backbone</li>
+          <li>Vue</li>
+        </ul>
+      </div>
+      // const frontEndFrameworks = [
+      //   'React',
+      //   'Angular',
+      //   'Ember',
+      //   'Knockout',
+      //   'Backbone',
+      //   'Vue'
+      // ];
+
+      // function Frameworks() {
+      //   const renderFrameworks = frontEndFrameworks.map((item) =>
+      //   <li key={item}>{item}</li>
+      // );
+      //   return (
+      //     <div>
+      //       <h1>Popular Front End JavaScript Frameworks</h1>
+      //       <ul>
+      //         {renderFrameworks}
+      //       </ul>
+      //     </div>
+      //   );
+      // };
+    ),
   },
   {
     path: "/example46",
-    main: () => <div></div>,
+    main: () => (
+      <div>
+        <h1>Current Online Users:</h1>
+        <ul>
+          <li>Jeff</li>
+          <li>Mary</li>
+          <li>Sara</li>
+          <li>Laura</li>
+        </ul>
+      </div>
+      // class MyComponent extends React.Component {
+      //   constructor(props) {
+      //     super(props);
+      //     this.state = {
+      //       users: [
+      //         {
+      //           username: 'Jeff',
+      //           online: true
+      //         },
+      //         {
+      //           username: 'Alan',
+      //           online: false
+      //         },
+      //         {
+      //           username: 'Mary',
+      //           online: true
+      //         },
+      //         {
+      //           username: 'Jim',
+      //           online: false
+      //         },
+      //         {
+      //           username: 'Sara',
+      //           online: true
+      //         },
+      //         {
+      //           username: 'Laura',
+      //           online: true
+      //         }
+      //       ]
+      //     }
+      //   }
+      //   render() {
+      //     const usersOnline = this.state.users.filter(user => user.online === true); // change code here
+      //     const renderOnline = usersOnline.map(user => <li key={user.username}>{user.username}</li>); // change code here
+      //     return (
+      //        <div>
+      //          <h1>Current Online Users:</h1>
+      //          <ul>
+      //            {renderOnline}
+      //          </ul>
+      //        </div>
+      //     );
+      //   }
+      // };
+    ),
   },
   {
     path: "/example47",
-    main: () => <div></div>,
+    main: () => (
+      <div></div>
+      // class App extends React.Component {
+      //   constructor(props) {
+      //     super(props);
+      //   }
+      //   render() {
+      //     return <div/>
+      //   }
+      // };
+      
+      // // Change code below this line
+      // ReactDOMServer.renderToString(<App />);
+    ),
   },
 ];
 
